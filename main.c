5341837a423debbb1887a1bbadfe4f15bce6013f
gilbenhamo <gilbh859@gmail.com>
2023-07-10 15:58:28 +0300

    Github rearrangement

---
+++ main.c
@@ -0,0 +1,37 @@
+///// Gil ben hamo /////
+
+
+#include "funcs.c"
+
+int main (int argc, char **argv) 
+{ 
+
+    int childPid; char * cmdLine;
+    parseInfo *info; 
+    printf("\033[0;36m################### Running custom shell ###################\n");
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\033[0m\n");
+    while(1)
+    {
+        printCurrentPath();
+        cmdLine= readline(" >> "); 
+        add_history(cmdLine);
+
+        info = parse(cmdLine);  // Parse information from user
+
+        if(!isPipe(info) && ifChangeDir(info))   // Change folder if its not pipe command
+            continue;
+
+        childPid = fork();   
+
+        if (childPid == 0)
+            {
+                /* child code */
+                executeCommand(info); //calls  execvp  
+            }
+        else 
+            { 
+                /* parent code */
+                waitpid(childPid);         
+            }	 
+    } 
+}
